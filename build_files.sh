b3d0f8bc2dfba3ffe985f7004b8a6f11a17350ea
Gourav Sen <sen9165812790@gmail.com>
2024-08-14 10:13:11 +0530

    Add files via upload

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 echo "BUILD START"
-python3.9 -m pip3 install -r requirements.txt
+
+# Install the dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
